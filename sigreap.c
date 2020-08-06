00dd6bd762904d1b468e43ee84f8fc14c970f353
Michael Wallner <mike@php.net>
2020-08-06 13:54:43 +0200

    sigreap: remove unneeded includes

--- sigreap.c
+++ sigreap.c
@@ -1,15 +1,11 @@
 #include <errno.h>
 #include <fcntl.h>
-#include <libgen.h>
 #include <limits.h>
 #include <signal.h>
-#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/prctl.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
